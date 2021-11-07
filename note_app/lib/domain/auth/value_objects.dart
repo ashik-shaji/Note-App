048fba3aa4182e48af1e40fd9b15185fe0a976e8
Ashik Shaji <ashikshaji604@gmail.com>
2021-11-07 23:02:30 +0530

    second commit

---
+++ note_app/lib/domain/auth/value_objects.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:note_app/domain/core/failures.dart';
+import 'package:note_app/domain/core/value_objects.dart';
+import 'package:note_app/domain/core/value_validators.dart';
+
+class EmailAddress extends ValueObject<String> {
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    return EmailAddress._(
+      validateEmailAddress(input),
+    );
+  }
+
+  const EmailAddress._(this.value);
+}
+
+class Password extends ValueObject<String> {
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    return Password._(
+      validatePassword(input),
+    );
+  }
+
+  const Password._(this.value);
+}
--- note_app/lib/domain/core/value_objects.dart
+++ note_app/lib/domain/core/value_objects.dart
@@ -2,7 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:note_app/domain/auth/email_address.dart';
+import 'package:note_app/domain/auth/value_objects.dart';
+import 'package:note_app/domain/core/failures.dart';
 
 @immutable
 abstract class ValueObject<T> {
