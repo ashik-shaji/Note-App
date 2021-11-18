2f6fd3ffc1f24697a32fa64d1bf6864ed55e2361
Ashik Shaji <ashikshaji604@gmail.com>
2021-11-18 23:29:58 +0530

    FirebaseAuthFacade implemented

--- note_app/lib/domain/auth/i_auth_facade.dart
+++ note_app/lib/domain/auth/i_auth_facade.dart
@@ -5,12 +5,12 @@ import 'package:note_app/domain/auth/value_objects.dart';
 
 abstract class IAuthFacade {
   Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
-    @required EmailAddress emailAddress,
-    @required Password password,
+    required EmailAddress emailAddress,
+    required Password password,
   });
   Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
-    @required EmailAddress emailAddress,
-    @required Password password,
+    required EmailAddress emailAddress,
+    required Password password,
   });
   Future<Either<AuthFailure, Unit>> signInWithGoogle();
 }
--- note_app/lib/domain/core/value_objects.dart
+++ note_app/lib/domain/core/value_objects.dart
@@ -3,12 +3,19 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:note_app/domain/auth/value_objects.dart';
+import 'package:note_app/domain/core/errors.dart';
 import 'package:note_app/domain/core/failures.dart';
 
 @immutable
 abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
+
+  /// Throws [UnexpectedValueError] containing the [ValueFailure]
+  T getOrCrash() {
+    // id = identity - same as writing (right) => right ( id -> (r) => r )
+    return value.fold((f) => throw UnexpectedValueError(f), id);
+  }
 
   bool isValid() => value.isRight();
 
